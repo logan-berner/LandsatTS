86a33c245cca68df996ffb146df18719f80b84e5
jakobjassmann <jakobjassmann@gmail.com>
2021-04-06 13:33:55 +0100

    fixed issues with test scripts

--- tests/testthat/test-lsat_get_pixel_centers.R
+++ tests/testthat/test-lsat_get_pixel_centers.R
@@ -9,7 +9,7 @@ test_that("lsat_get_pixel_centers works", {
                   -138.90298, 69.57986,
                   -138.90125, 69.58413),
                 ncol = 2, byrow = T)))
-  test_poly_sf <- sf::st_sf(st_sfc(test_poly, crs = 4326))
+  test_poly_sf <- sf::st_sf(sf::st_sfc(test_poly, crs = 4326))
 
   # Get pixel coordinates
   test_pixels <- lsat_get_pixel_centers(test_poly_sf)
@@ -18,8 +18,8 @@ test_that("lsat_get_pixel_centers works", {
   # save(test_pixels_control, file = "tests/data/lsat_get_pixel_centers.Rda")
 
   # Load control data
-  load("tests/data/lsat_get_pixel_centers.Rda")
+  load("../data/lsat_get_pixel_centers.Rda")
 
   # Test for equivalence
-  expect_equal(test_pixels, test_pixels_control)
+  testthat::expect_equal(test_pixels, test_pixels_control)
 })
