2dc1235df800eee174c46507fc244b81a4641b67
Logan Berner <logan.berner@nau.edu>
2021-12-01 15:21:17 -0700

    Updated so study area is delineated in this script and changed export to include Sept.

--- man/manuscript/scripts/disko_1_extract_lsat_data.R
+++ man/manuscript/scripts/disko_1_extract_lsat_data.R
@@ -22,22 +22,21 @@ setwd('C:/Users/Logan/My Drive/research/code/lsatTS/man/manuscript/')
 ee_Initialize()
 
 # Create a spatial polygon that demarcates the study area
-aoi.proj <- st_crs('+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs')
-
 aoi.poly <- st_polygon(list(matrix(
   c(-332950,-2243300,
     -334950,-2243300,
     -334950,-2245300,
     -332950,-2245300,
     -332950,-2243300),
-  ncol = 2, byrow = T))) %>%
-  st_sfc(crs = aoi.proj)
+  ncol = 2, byrow = T)))
+
+aoi.poly <- aoi.poly %>% st_sfc(crs = 3413) %>% st_transform(crs = 4326) %>% st_as_sf()
 
 # Get the central coordinates for each Landsat pixels in study area
-aoi.pts <- lsat_get_pixel_centers(aoi.poly)
+aoi.pts <- lsat_get_pixel_centers(aoi.poly, plot_map = T)
 
 # Extract a time-series of Landsat surface reflectance measurements for each Landsat pixel
-task_list <- lsat_export_ts(pixel_coords_sf = aoi.pts, startJulian = 142, endJulian = 243,
+task_list <- lsat_export_ts(pixel_coords_sf = aoi.pts, startJulian = 152, endJulian = 273,
                             file_prefix = 'disko', drive_export_dir = 'earth_engine')
 
 # END SCRIPT ----------------------------------------------------------------------------------------
