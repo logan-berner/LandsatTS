4b0f391f55d55458ab3eec801d061d86f6265c2c
jakobjassmann <jakobjassmann@gmail.com>
2022-04-13 10:53:40 +0100

    Commented out line in lsat_evaluate_phenological_max that threw an error. Plus, added missing si argument to final line in example.

--- R/lsat_evaluate_phenological_max.R
+++ R/lsat_evaluate_phenological_max.R
@@ -32,9 +32,9 @@
 #' lsat.dt <- lsat_general_prep(lsat.example.dt)
 #' lsat.dt <- lsat_clean_data(lsat.dt)
 #' lsat.dt <- lsat_calc_spec_index(lsat.dt, 'ndvi')
-#' # lsat.dt <- lsat_calibrate_rf(lsat.dt, band.or.si = 'ndvi', write.output = F)
+#' # lsat.dt <- lsat_calibrate_rf(lsat.dt, band.or.si = 'ndvi', write.output = FALSE)
 #' lsat.pheno.dt <- lsat_fit_phenological_curves(lsat.dt, si = 'ndvi') 
-#' lsat_evaluate_phenological_max(lsat.pheno.dt)
+#' lsat_evaluate_phenological_max(lsat.pheno.dt, si = 'ndvi')
 
 
 lsat_evaluate_phenological_max <- function(dt, 
