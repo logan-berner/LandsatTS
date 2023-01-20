efc6da110de14ae04ed47f47ca9dece0eb04e6a2
Logan <logan.berner@nau.edu>
2023-01-20 09:20:28 -0900

    changed lsatTS to LandsatTS

--- R/data.R
+++ R/data.R
@@ -2,8 +2,8 @@
 #' 
 #' A dataset containing Landsat surface reflectance measurements and ancillary
 #' data for six sample sites in the Arctic. These data are used for the examples
-#' included with lsatTS. 
+#' included with LandsatTS. 
 #' 
 #' @format A data.table with 5296 rows and 23 variables
-#' @source Generated using example code provided in lsatTS::lsat_export_ts()
+#' @source Generated using example code provided in LandsatTS::lsat_export_ts()
 "lsat.example.dt"
