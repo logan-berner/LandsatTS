d60d477bfae6c564a2b6c47aab2f6a0e98dd1877
Logan <logan.berner@nau.edu>
2023-03-08 09:19:28 -0900

    documenting datasets

--- R/data.R
+++
@@ -1,9 +0,0 @@
-#' Landsat surface reflectance for six sample sites in the Arctic
-#' 
-#' A dataset containing Landsat surface reflectance measurements and ancillary
-#' data for six sample sites in the Arctic. These data are used for the examples
-#' included with LandsatTS. 
-#' 
-#' @format A data.table with 5296 rows and 23 variables
-#' @source Generated using example code provided in LandsatTS::lsat_export_ts()
-"lsat.example.dt"
