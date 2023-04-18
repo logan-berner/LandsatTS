b42b1ba5bc8b89cbf28f03b4f6f5f7a968625bce
Logan <logan.berner@nau.edu>
2023-04-18 09:39:15 -0800

    script to set global variables

---
+++ R/global_variables.R
@@ -0,0 +1,16 @@
+# SETS GLOBAL VARIABLES FOUND IN PACKAGE DURING DEVTOOLS::CHECK()
+
+utils::globalVariables(c(".", "..keep.bands", "LANDSAT_7", "N" ,"Processing",
+  "abs.zscore", "avg", "blue", "clear",
+  "cloud.cover", "date.acquired", "dif.pcnt", "dif.pcnt.rank", "doy", "evi", "evi2",
+  "fig.list", "first.yr", "first.yr.abs.dif", "geometric.rmse.model", "gndvi", "green",
+  "intercept", "jrc.water", "kndvi", "last.yr", "last.yr.abs.dif", "latitude", "longitude",
+  "max.doy", "min.doy", "msi", "n", "n.obs", "n.obs.15days", "n.obs.fac", "n.obs.focal.win",
+  "n.obs.gs", "n.q025", "n.q500", "n.q975", "n.yr.obs", "n.yrs.from.focal", "nbr", "ndii", "ndmi",
+  "ndvi", "ndvsi", "ndwi", "nir", "nirv", "obs.id", "pcnt", "pcnt.dif", "psri", "pull", "pval",
+  "qa.pixel", "qa.radsat", "red", "sample.id", "satellite", "satvi", "savi", "si.adjustment",
+  "si.cor.pcntdif", "si.gs.q90", "si.max", "si.max.cor", "si.max.obs", "si.max.pred",
+  "si.max.uncor", "si.uncor.pcntdif", "slope", "snow", "spl.fit", "spl.fit.max",
+  "spl.fit.max.doy", "spl.frac.max", "sr.b6", "std", "sun.elevation", "swir1", "swir2",
+  "total.change", "total.change.pcnt", "trend.cat", "trend.period", "variable", "water",
+  "wdrvi", "x", "xcal", "year.rescale", ":=", "!!"))
